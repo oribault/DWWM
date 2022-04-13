7b960b28ca36d1a97cf4d8e250985c3a4869cfa6
Océane <flamme.noire@live.fr>
2022-04-13 16:44:12 +0200

    validation

--- Javascript/Exercice 2/heure.js
+++ Javascript/Exercice 2/heure.js
@@ -1,4 +1,8 @@
-let chainedate = "";//
-let madate = new Date();//
-chainedate += madate.getDate() + "/"+madate.getMonth() + "/" + madate.getFullYear();//
-document.getElementById("txtdate").value = chainedate;//
+let chaineDate = "";//Créer une variable chaine(local)
+let madate = new Date();// Instanciation objet Date
+chaineDate += madate.getDate() + "/"+ (madate.getMonth()+1) + "/" + madate.getFullYear();// Utilisation des méthodes de l'objet Date
+document.getElementById("txtdate").value = chaineDate;// Recherche la la valeur de l'input de type texte et on lui affecte la variable
+
+let chaineHeure ="" + madate.getHours() + ":" + madate.getMinutes() + ":" + madate.getSeconds();
+document.getElementById("txtheure").value = chaineHeure;
+
