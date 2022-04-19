99395c7f2cbb42901be9ce40504044e5795dccd0
Océane <flamme.noire@live.fr>
2022-04-19 15:53:11 +0200

    Nouveau changements

--- Javascript/Exercice 2/heure.js
+++ Javascript/Exercice 2/heure.js
@@ -1,8 +1,35 @@
+// Afficher les la Date
 let chaineDate = "";//Créer une variable chaine(local)
 let madate = new Date();// Instanciation objet Date
-chaineDate += madate.getDate() + "/"+ (madate.getMonth()+1) + "/" + madate.getFullYear();// Utilisation des méthodes de l'objet Date
-document.getElementById("txtdate").value = chaineDate;// Recherche la la valeur de l'input de type texte et on lui affecte la variable
 
-let chaineHeure ="" + madate.getHours() + ":" + madate.getMinutes() + ":" + madate.getSeconds();
-document.getElementById("txtheure").value = chaineHeure;
+let jour=    (madate.getDate()<10) ? "0"+madate.getDate() : madate.getDate();
+let mois= (madate.getMonth()+1<10 ) ? "0"+(madate.getMonth()+1): madate.getMonth()+1  ; 
+let annee= madate.getFullYear();     
+
+chaineDate += jour + "/"+  mois + "/" + annee;// Utilisation des méthodes de l'objet Date
+
+let monBouton= document.getElementById("btndate");
+monBouton.addEventListener("click", function() 
+{ 
+document.getElementById("txtdate").value=chaineDate;
+} )
+
+//Afficher les Heures
+let chaineHeure ="";
+let monHeure = new Date();
+
+let second =(monHeure.getSeconds()<10) ? "0" +monHeure.getSeconds() : monHeure.getSeconds();
+let minutes = (monHeure.getMinutes()<10) ? "0" +monHeure.getMinutes(): monHeure.getMinutes();
+let heures = (monHeure.getHours() <10) ? "0" +monHeure.getHours() : monHeure.getHours();
+
+chaineHeure += heures + ":" + minutes + ":" + second;
+
+let monButton = document.getElementById("btnheure");
+monButton.addEventListener("click", function(){
+document.getElementById("txtheure").value=chaineHeure;
+})
+
+
+
+
 
