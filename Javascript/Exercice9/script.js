f9b46cfaefe5c32d22b23529cec46aab23ad0e75
Océane <flamme.noire@live.fr>
2022-04-25 11:22:59 +0200

    Nouveau changement

--- Javascript/Exercice9/script.js
+++ Javascript/Exercice9/script.js
@@ -2,6 +2,42 @@ function getColor() {
 	let chainecouleur = "#" + document.querySelector("#rouge").value + document.querySelector("#vert").value + document.querySelector("#bleu").value;
 	document.body.setAttribute("style"," background-color:" + chainecouleur)
 }
+
+const rouge = document.getElementById("rouge");	
+const vert = document.getElementById("vert");
+const bleu = document.getElementById("bleu");
+
+
+	function verifChamps(_champs)
+{
+	let testbool = false
+	let chaine = _champs.value.toUpperCase();
+	let regex = /^[0-9A-F]{2}$/;
+	if (chaine.match(regex)){
+		
+		testbool = true
+	}else {
+		_champs.value = "";
+		_champs.focus();
+	}
+	return testbool;
+}
+document.querySelector("#rouge").addEventListener("blur", function () {
+	if (verifChamps(rouge)) {
+		getColor();
+	}
+});
+
+document.querySelector("#vert").addEventListener("blur", function () {
+	if (verifChamps(vert)) {
+		getColor();
+	}
+});
+document.querySelector("#bleu").addEventListener("blur", function () {
+	if (verifChamps(bleu)) {
+		getColor();
+	}
+});
 	let btnrouge = document.querySelector("#btrouge");
 	btnrouge.addEventListener("click", function () {
 		
@@ -28,4 +64,23 @@ function getColor() {
 		document.querySelector("#bleu").value = "ff";
 		getColor();
 	
-});
+		});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
